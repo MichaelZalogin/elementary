781212b1f1151ce4b393d772effa1dcb7b1fd4b4
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-23 16:56:53 +0300

    Сумма чисел. [#226416 #324724], Подсчет суммы чётных чисел в диапазоне [#192 #324996]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,6 +7,24 @@ import org.junit.Assert;
 public class CounterTest {
 
     @Test
+    public void whenSumNumbersFromZeroToFiveThenFifteen() {
+        int start = 0;
+        int finish = 5;
+        int result = Counter.sum(start, finish);
+        int expected = 15;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenSumNumbersFromFiveToTenThenFortyFive() {
+        int start = 5;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 45;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         int start = 1;
         int finish = 10;
