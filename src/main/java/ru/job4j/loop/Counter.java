af95cd0037dfd843b18d45c3009c725eba3f9bfd
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-23 16:47:48 +0300

    Сумма чисел. [#226416 #324724]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,18 +3,8 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (start = 0; start <= finish; start++) {
+        for (int i = start; i <= finish; i++) {
             sum = sum + start;
-        }
-        return sum;
-    }
-
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int i = 0; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
         }
         return sum;
     }
