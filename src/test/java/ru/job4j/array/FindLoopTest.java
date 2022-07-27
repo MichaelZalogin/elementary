d2de9eee8c16021d9e21d041e18d070b2d7e93ca
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-27 03:10:47 +0300

    Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,9 +1,8 @@
 package ru.job4j.array;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FindLoopTest {
 
@@ -13,7 +12,7 @@ class FindLoopTest {
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -22,6 +21,30 @@ class FindLoopTest {
         int el = 10;
         int result = FindLoop.indexOf(data, el);
         int expected = -1;
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        /* Дописать тест, когда элемент 8 в диапазоне не найден.
+        Можно искать любой другой элемент, корректно отражая этот факт в названии теста. */
+        int[] data = {5, 4, 3, 2, 1, 10, 12, 8, 22};
+        int el = 1;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
     }
 }
