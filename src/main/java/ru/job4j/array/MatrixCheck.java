a531cfad1c50fe2a68601fa653555f07fc497fc3
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-29 01:46:44 +0300

    Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,20 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i <= board.length - 1; i++) {
+            if (board[i][i] == 'X') {
+                if (monoVertical(board, i)) {
+                    result = true;
+                    break;
+                } else {
+                    result = monoHorizontal(board, i);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
