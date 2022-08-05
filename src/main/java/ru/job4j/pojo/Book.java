bf9bc3416b8b7c896910218ec61d3df88613aef6
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-06 00:13:21 +0300

    Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int pageCount;
+
+    public Book(String name, int pageCount) {
+        this.name = name;
+        this.pageCount = pageCount;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPageCount() {
+        return this.pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Name1", 23);
+        Book book2 = new Book("Name2", 54);
+        Book book3 = new Book("Name3", 64);
+        Book book4 = new Book("Clean Code", 21);
+        Book[] books = {book1, book2, book3, book4};
+        for (int i = 0; i < books.length; i++) {
+            Book tmp = books[i];
+            System.out.println("Name book: " + tmp.getName() + ", page count = " + tmp.getPageCount());
+        }
+        Book book5 = book1;
+        books[0] = books[3];
+        books[3] = book1;
+        for (int i = 0; i < books.length; i++) {
+            Book tmp = books[i];
+            System.out.println("Name book: " + tmp.getName() + ", page count = " + tmp.getPageCount());
+        }
+        for (int i = 0; i < books.length; i++) {
+            Book tmp = books[i];
+            if (tmp.getName().equals("Clean Code")) {
+                System.out.println("Name book: " + tmp.getName() + ", page count = " + tmp.getPageCount());
+            }
+        }
+    }
+}
