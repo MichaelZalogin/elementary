4914c046cc5c4bd590703cc8083cdd6d0a5e02dd
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-17 02:38:50 +0300

    Иерархия исключений и множественный catch. [#219368 #332755]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,13 +2,10 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        int i = 0;
         if (n == 0 || n == 1) {
-            i = 1;
-        } else {
-            i = calc(n - 1) * n;
+            return 1;
         }
-        return i;
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
