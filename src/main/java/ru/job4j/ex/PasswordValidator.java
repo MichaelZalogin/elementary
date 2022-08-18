75e79606342d0c133dd2e4f46ad30e30ae8883d0
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-19 01:36:24 +0300

    Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/ex/PasswordValidator.java
@@ -0,0 +1,96 @@
+package ru.job4j.ex;
+
+import java.util.Set;
+
+public class PasswordValidator {
+
+    public static boolean findUpperSymbol(String upperValidate) {
+        for (int i = 0; i < upperValidate.length(); i++) {
+            char tmp = upperValidate.charAt(i);
+            if (Character.isUpperCase(tmp)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean findLowerSymbol(String lowerValidate) {
+        for (int i = 0; i < lowerValidate.length(); i++) {
+            char tmp = lowerValidate.charAt(i);
+            if (Character.isLowerCase(tmp)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean findDigits(String digitsValidate) {
+        for (int i = 0; i < digitsValidate.length(); i++) {
+            char tmp = digitsValidate.charAt(i);
+            if (Character.isDigit(tmp)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean findPrimitivePass(String primitivePassword) {
+        primitivePassword = primitivePassword.toLowerCase();
+        String[] primitivePasswords = {"qwerty123#", "123456789", "password"};
+        for (int i = 0; i < primitivePasswords.length; i++) {
+            if (primitivePassword.equals(primitivePasswords[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean findSpecialSymbols(String specialValidate) {
+        Set<Character> allowedCharacter = Set.of('(', ')', '-', '!', '?', '#', '@', '*', ']', '[');
+        for (int i = 0; i < specialValidate.length(); i++) {
+            char tmp = specialValidate.charAt(i);
+            if (allowedCharacter.contains(tmp)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String validate(String password) {
+
+        if (password == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (password.length() > 33 || password.length() < 7) {
+            return "Длина пароля должна находится в диапазоне от 8 до 32 символов";
+        }
+
+        if (!findUpperSymbol(password)) {
+            return "Пароль должен содержать хотя бы один символ в верхнем регистре";
+        }
+
+        if (!findLowerSymbol(password)) {
+            return "Пароль должен содержать хотя бы один символ в нижнем регистре";
+        }
+
+        if (!findDigits(password)) {
+            return "Пароль должен содержать хотя бы одну цифру";
+        }
+
+        if (!findSpecialSymbols(password)) {
+            return "Пароль должен содержать хотя бы один специальный символ";
+        }
+
+        if (findPrimitivePass(password)) {
+            return "Пароль должен быть сложнее";
+        }
+
+        return password;
+    }
+
+    public static void main(String[] args) {
+        String s = "QweRty123#";
+        System.out.println(validate(s));
+    }
+}
---
+++ src/test/java/ru/job4j/ex/PasswordValidatorTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    public void whenLengthLess8() {
+        String result = PasswordValidator.validate("dwW3re");
+        String expected = "Длина пароля должна находится в диапазоне от 8 до 32 символов";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenLengthMore32() {
+        String result = PasswordValidator.validate("dewfewe3fewdewfewe3fewdewfewe3fewewq");
+        String expected = "Длина пароля должна находится в диапазоне от 8 до 32 символов";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenNonUpperSymbol() {
+        String result = PasswordValidator.validate("somewords");
+        String expected = "Пароль должен содержать хотя бы один символ в верхнем регистре";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenNonLowerSymbol() {
+        String result = PasswordValidator.validate("SOMEWORDS");
+        String expected = "Пароль должен содержать хотя бы один символ в нижнем регистре";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenNotContainsDigits() {
+        String result = PasswordValidator.validate("SOMEWfewfew");
+        String expected = "Пароль должен содержать хотя бы одну цифру";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenNotContainsSpecialSymbol() {
+        String result = PasswordValidator.validate("SOMEWfewfew32");
+        String expected = "Пароль должен содержать хотя бы один специальный символ";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenPasswordVerySimple() {
+        String result = PasswordValidator.validate("QweRty123#");
+        String expected = "Пароль должен быть сложнее";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    public void whenPasswordAsserted() {
+        String result = PasswordValidator.validate("Q2dE#ew123#");
+        String expected = "Q2dE#ew123#";
+        assertThat(expected).isEqualTo(result);
+    }
+}
