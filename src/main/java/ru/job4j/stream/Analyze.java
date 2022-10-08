e3e2912573c25747132f02f4ad2ef5ec22a5179b
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-09 00:36:06 +0300

    Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(Double.NaN);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+                .map(pupil -> new Tuple(pupil.name(), pupil.subjects().stream()
+                .mapToInt(Subject::score)
+                        .average()
+                        .orElse(Double.NaN)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return null;
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return null;
+    }
+}
